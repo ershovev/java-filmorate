585f1301f1158815dfc4b749b3e0f79bd627816e
Ershow <me@evershov.com>
2023-04-25 12:46:45 +0300

    Исправить стиль.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,13 +5,17 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
-    Film create (Film film); // добавить фильм
-    Film update (Film film); // обновить фильм
+    Film create(Film film); // добавить фильм
+
+    Film update(Film film); // обновить фильм
+
     List<Film> findAll(); // получить список всех фильмов
+
     boolean isFilmPresent(Film film); // присутствует ли фильм в базе
 
     boolean isFilmPresentById(Integer id);
 
     Film getFilm(Integer id); // получить фильм
+
     List<Film> getAllFilms(); // получить все фильмы
 }
