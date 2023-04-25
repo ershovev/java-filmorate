f86dec389853cacde987ff8cd8f89d6f439d8b6c
Ershow <me@evershov.com>
2023-04-25 12:49:14 +0300

    Исправить стиль.

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,8 +5,8 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
-
     List<User> findAll(); // получение всех пользователей
+
     User create(User user); // создание пользователя
 
     User update(User user); // обновление пользователя
